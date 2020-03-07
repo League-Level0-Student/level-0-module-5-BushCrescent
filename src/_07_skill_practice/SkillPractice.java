c667fa1ed4d5d369103ab5b79494a7f5e059d731
League Student <league@iMac-13.attlocal.net>
2020-03-07 12:54:40 -0800

    module 5

---
+++ src/_07_skill_practice/SkillPractice.java
@@ -0,0 +1,106 @@
+package _07_skill_practice;
+import javax.swing.JOptionPane;
+import java.util.Random;
+public class SkillPractice {
+	public static void main(String[] args) {
+
+		SkillPractice skills = new SkillPractice();
+		           //skills.skill1();
+		           //skills.skill2();
+		           skills.skill3();
+		           skills.skill4();
+		           skills.skill5();
+		}
+
+		void skill1() {
+		// Use pop-ups for the following.
+		// Ask the user how many dimes they have
+String dimes = JOptionPane.showInputDialog("How many dimes do you have?");
+int cents = Integer.parseInt(dimes);
+
+		// Tell them how many cents they have (hint multiply by 10)
+
+JOptionPane.showMessageDialog(null, "You have " + cents*10 + " cents");
+
+
+		 //Ask the user how tall they are (inches)
+String height = JOptionPane.showInputDialog("How tall are you?");
+
+int h = Integer.parseInt(height);
+
+		// If they are shorter than 36 inches, tell them to eat their Wheaties
+if (h < 36) {
+	JOptionPane.showMessageDialog(null, "Eat your Wheaties!");
+}
+
+
+
+		}
+
+		void skill2() { // Write a loop to print every third number between 1 and 30 to the console
+for (int i=1; i<31; i++) {
+	System.out.println(i);
+}
+
+
+
+
+
+
+		}
+
+		void skill3() { // Get a random number that is less than 20 and print it to the console
+
+
+		// Get another random number that is less than 10 and print it to the console
+Random ran = new Random();
+int x = ran.nextInt(20);
+System.out.println(x);
+
+Random y = new Random(10);
+int z = y.nextInt(10);
+System.out.println(z);
+
+
+		// Using a pop-up, tell the user the difference between the numbers // Hint: use subtraction
+JOptionPane.showMessageDialog(null, "The difference between those numbers are " + (z-x));
+
+
+		}
+
+		void skill4() { // In a pop-up, ask the user for the city they live in
+
+
+
+		// If they answered "San Diego", tell them they live in America's Finest City
+
+
+
+		// Otherwise, tell them to move to San Diego
+
+
+
+		// Create a variable - cars - and initialize it to the number of cars your family has. // If there are 0 cars, use a pop-up to display, "I bet you use public transportation."
+
+
+
+		// If there is 1 car, use a pop-up to display the make/model of the car
+
+
+
+		// If there is more than 1 car, use a pop-up to display how many wheels the // cars have between them.
+
+
+
+		}
+
+		void skill5() { // In a pop-up, ask the user for the name of their school
+
+
+
+		// In another pop-up, tell the user, that their school is a fantastic school. // You must include the name of the school in the message.
+
+
+
+		}
+}
